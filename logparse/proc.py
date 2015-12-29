4b7fb6312743757a063d35e6d4ae6c0421f0e95a
云心逸 <yunxinyi+github@gmail.com>
2015-12-29 16:52:06 +0800

    use queue to communicate

--- logparse/proc.py
+++ logparse/proc.py
@@ -25,10 +25,8 @@ import os
 import re
 import sys
 import time
-import pprint
 import operator
 import functools
-import threading
 
 import FastInt
 
@@ -53,44 +51,44 @@ def timefunc(func):
 #NOTICE: 12-21 00:00:00:  imas * 26706 [ÄÚÒÂµê] qry=ÄÚÒÂµê ip=223.98.252.57 rt_ip=10.128.205.21 pn=0 tn= pre=0 s=327126f4b415f974 bd=AD4890C25E3E24A41E13CF3FE2BACB44 cuid=E382D676F6FFB6674CAC94DF6C8DECD9|789195320493468 imei= idm=(2,2,0) src=915, fn=cnkang_cpr tm=(128|11447|16998|8770|0|0|0|0|0|0|0|0|0|0|0|0|15728|0|179|16421|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|9307|0|0|0|0|0|0|0|0|0|0|0|0) tt=79203 sp=(51985,8388608) inner=1 ft=37 ppq=0 csfn=(6|9) bsrn=(47|47,) amrn=47 qspn=2 ws=1 di=1(47/47|47) qs_kpi=(0|2|0) it=(0|15480|11339|16111|0|0|15152|0|0|7909|0|0|0|0|0) mem=(5)([18168263][0][0][0][0] 32M[0]) pack=333414 tmo=0 qs_cmd_time=() extra_log=fetch:9,before_write:69852,fengsui:1,all_except_conn:79216, 
 class Task(mp.Process):
     pattern = re.compile(r'tm=\(([^)]+)\) tt=(\d+).*qspn=(\d+)')
-    def __init__(self, lines):
+    def __init__(self, queue, lines):
         super(Task, self).__init__()
+        self._queue = queue
         self._lines = lines
-        self._NR = 0
-        self._ST = [0] * 54
-
-    @property
-    def nrecord(self):
-        return self._NR
-
-    @property
-    def stat(self):
-        return self._ST
 
     def run(self):
+        PAT = Task.pattern
+        NR = 0
+        ST = [0] * 54
+
         for line in self._lines:
             try:
-                m = Task.pattern.search(line)
+                m = PAT.search(line)
                 #FastInt.add(self._CNT, '|'.join(m.groups()))
-                self._ST = map(operator.add, self._ST,
+                ST = map(operator.add, ST,
                         map(FastInt.toInt, ('|'.join(m.groups())).split('|')))
-                self._NR += 1
+                NR += 1
             except Exception as e:
                 print >> sys.stderr, '*** malformed line {0}'.format(e)
+        self._queue.put([NR] + ST)
 
 @timefunc
 def parse(lines, nproc):
     nline = len(lines)
     step = (nline + nproc - 1)/nproc
-    taskq = map(Task, [lines[i:i+step] for i in range(0, nline, step)])
+    queue = mp.Queue()
+    taskq = map(lambda lines : Task(queue, lines),
+            [lines[i:i+step] for i in range(0, nline, step)])
     for task in taskq:
         task.start()
         print '+ {0}'.format(task)
     for task in taskq:
         task.join()
         print '- {0}'.format(task)
-    statq = [task.stat for task in taskq]
+    statq = [queue.get() for task in taskq]
     stat = reduce(operator.add, map(np.array, statq))
+    NR = stat[0]
+    stat = np.array(stat[1:])/float(NR)
     print 'report'.center(80, '-')
     print stat
     print ''.center(80, '=')
