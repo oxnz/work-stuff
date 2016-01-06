189021cd0e5d5b987e91afa20d253ca4f3a94254
unknown <zhangpan05@6679CC50E17C5FA.internal.baidu.com>
2016-01-06 18:37:31 +0800

    init

---
+++ spring-demo/Test/src/main/java/com/errpro/Test/entity/Product.java
@@ -0,0 +1,37 @@
+package com.errpro.Test.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by zhangpan05 on 2016/1/5.
+ */
+@Entity
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
